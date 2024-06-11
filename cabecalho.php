19093aa962544ff77c86cdd4113f4d4f9dfafc7a
Allan <allan1521lima@gmail.com>
2024-06-10 22:25:34 -0300

    Imobiliaria

---
+++ cabecalho.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+
+    <title>Imobiliária</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+    <!-- Example Code -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-custom">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Home</a>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Cadastro
+                </a>
+                <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="cadastro_imovel.php">Cadastrar Imóvel</a></li>
+
+                </ul>
+                </li>
+                </ul>
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Administrativo
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Alugar Livro</a></li>
+                    <li><a class="dropdown-item" href="#">Devolver Livro</a></li>
+
+                </ul>
+                </li>
+                </ul>
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    Relatório
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Listar Livros</a></li>
+                    <li><a class="dropdown-item" href="#">Listar Livros</a></li>
+                    <li><a class="dropdown-item" href="#">Relatório de Devolução</a></li>
+                </ul>
+                </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <!-- End Example Code -->
+</body>
+
+</html>
